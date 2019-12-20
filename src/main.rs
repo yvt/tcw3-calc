b56bd8b2aaa49d660393a59523c955ba26b545c6
yvt <i@yvt.jp>
2019-12-20 12:00:53 +0900

    Implement the behavior

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::cell::Cell;
 use log::debug;
 use tcw3::{
     pal,
@@ -12,7 +11,109 @@ tcw3_calc_meta::designer_impl! { crate::CalcButton }
 
 impl MainView {
     fn handle_op(&self, op: char) {
-        dbg!(op);
+        let mut calc_state = self.calc_state();
+        calc_state.op(op);
+        self.set_calc_state(calc_state);
+    }
+}
+
+#[derive(Clone, PartialEq)]
+struct CalcState {
+    value: String,
+    operand: f64,
+    operator: char,
+    in_num: bool,
+}
+
+impl CalcState {
+    fn new() -> Self {
+        Self {
+            value: "0".to_string(),
+            operand: 0.0,
+            operator: 'C',
+            in_num: false,
+        }
+    }
+
+    fn display(&mut self) {
+        // TODO: change hyphen-minus to actual minus
+        self.value = self.operand.to_string();
+    }
+
+    fn compute(&mut self) {
+        if self.in_num {
+            let operand2 = self.value.parse().unwrap_or(0.0);
+            let result = match self.operator {
+                '+' => Some(self.operand + operand2),
+                '−' => Some(self.operand - operand2),
+                '×' => Some(self.operand * operand2),
+                '÷' => Some(self.operand / operand2),
+                _ => None,
+            };
+            if let Some(result) = result {
+                self.operand = result;
+                self.display();
+                self.in_num = false;
+            }
+        }
+    }
+
+    fn op(&mut self, op: char) {
+        match op {
+            '0'..='9' => {
+                if !self.in_num {
+                    self.value.clear();
+                    self.in_num = true;
+                }
+                self.value.push(op);
+            }
+            '+' | '−' | '×' | '÷' | '=' => {
+                self.compute();
+                self.operand = self.value.parse().unwrap_or(0.0);
+                self.operator = op;
+                self.in_num = false;
+            }
+            '±' => {
+                if self.in_num {
+                    if self.value.starts_with('−') {
+                        self.value = self.value[3..].to_string();
+                    } else {
+                        self.value = ["−", &self.value].concat();
+                    }
+                } else {
+                    self.operand = -self.operand;
+                    self.display();
+                }
+            }
+            '.' => {
+                if !self.in_num {
+                    self.value = "0".to_string();
+                    self.in_num = true;
+                }
+                if self.value.find('.').is_none() {
+                    self.value.push('.');
+                }
+            }
+            'c' => {
+                self.value = "0".to_string();
+                self.in_num = false;
+            }
+            'C' => {
+                self.value = "0".to_string();
+                self.operator = 'C';
+                self.in_num = false;
+            }
+            '⌫' => {
+                if self.in_num {
+                    self.value.pop();
+                    if self.value.is_empty() || self.value == "−" {
+                        self.value = "0".to_string();
+                        self.in_num = false;
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
